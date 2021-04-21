09c33e0988737ad8b072c4a4b7560b377a922f2e
vikus <10.20.46.13v@gmail.com>
2021-04-21 13:54:28 +0300

    add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
 
 
 import App from './App';
 import { store } from './store/store';
+import {theme} from './theme';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
